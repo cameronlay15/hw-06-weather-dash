e761d8bae20fc004a7f6c46ec07903c17fd232f6
Cameron Lay <cameronlay15@gmail.com>
2020-09-14 22:16:15 -0400

    creating variables

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+ //  creates links/buttons https://getbootstrap.com/docs/4.0/components/list-group/
+ cityListButton = $("<a>").attr({
+    class: "list-group-item list-group-item-action",
+    href: "#",
+  });
+  // appends history as a button below the search field
+  cityListButton.text(searchHistory[i]);
+  $(".list-group").append(cityListButton);
+}
+}
+var city;
+var mainCard = $(".card-body");
+// invokes getItems
+getItems();
+// main card
+
 function getData() {
     var queryURL =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -10,5 +26,27 @@ function getData() {
       url: queryURL,
       method: "GET",
     }).then(function (response){
-        
-    }
+        // using moment to craft the date
+        var date = moment().format(" MM/DD/YYYY");
+        // takes the icon code from the response and assigns it to iconCode
+        var iconCode = response.weather[0].icon;
+        // builds the main card icon url
+        var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+        // takes the name added from the search and the date/format from moment and creates a single var
+        var name = $("<h3>").html(city + date);
+        // displays name in main card
+        mainCard.prepend(name);
+        // displays icon on main card
+        mainCard.append($("<img>").attr("src", iconURL));
+        // converts K and removes decimals using Math.round
+        var temp = Math.round((response.main.temp - 273.15) * 1.8 + 32);
+        mainCard.append($("<p>").html("Temperature: " + temp + " &#8457"));
+        var humidity = response.main.humidity;
+        mainCard.append($("<p>").html("Humidity: " + humidity));
+        var windSpeed = response.wind.speed;
+        mainCard.append($("<p>").html("Wind Speed: " + windSpeed));
+        // takes from the response and creates a var used in the next request for UV index
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+        // separate request for UV index, requires lat/long
+    
